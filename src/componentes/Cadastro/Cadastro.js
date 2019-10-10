751443c4c60ee8189bee195f5b24d7e502323ef0
danilowOENS <dl539994@gmail.com>
2019-10-10 18:01:00 -0300

    adicionei ao banco de dados

--- src/componentes/Cadastro/Cadastro.js
+++ src/componentes/Cadastro/Cadastro.js
@@ -7,15 +7,50 @@ const Cadastro = () => {
   const [email, setEmail] = useState("");
   const [confirmEmail, setConfirmEmail] = useState("");
   const [senha, setSenha] = useState("");
+  const [count, setCount ] = useState(1);
+  const [mensagem, setMensagem] = useState("");
+  const [status, setStatus] = useState()
+
+const resposta = texto => {
+  setMensagem(texto);
+  setTimeout(() => {
+    setMensagem("")
+  }, 3000);
+}
 
   const handleSubmit = e => {
     e.preventDefault();
+
+   
+    if (email === confirmEmail) {
+      const payload = {
+      name:nome,
+      email:email,
+      confirm_email: confirmEmail,
+      password: senha
+    }
+
+    localStorage.setItem(`Dados${count}`, JSON.stringify(payload));
+    setCount(count + 1)
+
+    setNome("");
+    setEmail("");
+    setConfirmEmail("");
+    setSenha("");
+    setStatus(true)
+    resposta("cadastro com sucesso")
+
+  } else {
+    resposta("Os emails não correspondem");
+    setStatus(false);
+    }
   };
 
 
   return (
     <div className="Cadastro">
-      <h1>Faça seu cadastro</h1>
+      <h1>faça seu cadastro</h1>
+      <p style={status ? {color: "green"} : {color: "red"}}>{mensagem}</p>
       <form onSubmit={handleSubmit}>
         <Input
           value={nome}
